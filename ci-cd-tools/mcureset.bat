c32ad5550d76c83386a095e5a3173de509028122
Duc.VuMinh <duc.vuminh@meiko.vn>
2024-01-10 14:16:24 +0700

    Jenkins OK V1.1

--- ci-cd-tools/mcureset.bat
+++ ci-cd-tools/mcureset.bat
@@ -2,4 +2,4 @@ setlocal
 
 set "flash_tool=C:\Program Files\STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI"
 
-"%flash_tool%" --connect port=SWD sn=066BFF575550897767162155 -hardRst
+"%flash_tool%" --connect port=SWD sn=55FF70066685485510331367 -hardRst
