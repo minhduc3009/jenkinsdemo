78cc4dacec3a28509d3ce5a118169831691880cb
Duc.VuMinh <duc.vuminh@meiko.vn>
2024-01-10 10:49:11 +0700

    demo

--- Debug/App/gpio-app/subdir.mk
+++ Debug/App/gpio-app/subdir.mk
@@ -15,13 +15,6 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-App/gpio-app/%.o: ../App/gpio-app/%.c App/gpio-app/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
-
-clean: clean-App-2f-gpio-2d-app
-
-clean-App-2f-gpio-2d-app:
-	-$(RM) ./App/gpio-app/app_main.d ./App/gpio-app/app_main.o
-
-.PHONY: clean-App-2f-gpio-2d-app
+App/gpio-app/app_main.o: ../App/gpio-app/app_main.c App/gpio-app/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"App/gpio-app/app_main.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
--- ci-cd-tools/flashi.bat
+++ ci-cd-tools/flashi.bat
@@ -6,8 +6,8 @@ setlocal
 set "build_type=Debug"
 if not [%1]==[] set "build_type=%1"
 
-set "ws_root=C:\Users\gene\Documents\Gene\proj\STM32CubeIDE\workspace_1.6.1\ci-cd-class-1"
-set "sn=066BFF575550897767162155"
+set "ws_root=C:\Users\Admin\Downloads\ci-cd-class-1"
+set "sn=55FF70066685485510331367"
 set "image_file=%ws_root%\%build_type%\ci-cd-class-1.bin"
 
 "%ws_root%\ci-cd-tools\flash.bat" %sn% "%image_file%"
